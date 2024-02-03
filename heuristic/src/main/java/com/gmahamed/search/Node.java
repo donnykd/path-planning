19d31a9c6bb3fc87714d6d45c35eccd452bf4a8f
Khalid <zkac429@live.rhul.ac.uk>
2024-02-03 10:31:22 +0000

    Started my A* search implementation
    Created a test

---
+++ heuristic/src/main/java/com/gmahamed/search/Node.java
@@ -0,0 +1,12 @@
+package com.gmahamed.search;
+
+public class Node {
+    public int i, j;
+    Node parent;
+    int f, g, h;
+
+    public Node(int i, int j){
+        this.i = i;
+        this.j = j;
+    }
+}
---
+++ heuristic/src/test/java/com/gmahamed/search/NodeTest.java
@@ -0,0 +1,12 @@
+package com.gmahamed.search;
+import org.junit.jupiter.api.Test;
+
+public class NodeTest {
+    Node node;
+
+    @Test
+    void testSetup(){
+        node = new Node(1, 1);
+    }
+
+}
